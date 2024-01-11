77bb80935da0889e8adbe60e5b2dd1ca340cf36f
Celestin Mbuyamba <celembuyamba@gmail.com>
2024-01-11 08:46:02 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import SockJsClient from 'react-stomp';
 
-function App() {
+const App = () => {
+
+  const [message, setMessage] = useState('');
+  const [topics, setTopics] = useState([]);
+
+  const onSendBtnClick = () => {
+    axios.get("http://192.168.1.104:8080/api?oid=1.3.6.1.4.1.1206.4.2.3.9.8.1.0")
+      .then(() => {
+        console.log("Request Sent");
+      }
+      ).catch(() => {
+        console.error("Erroe");
+      });
+  }
+
+  const SOCKET_URL = 'http://192.168.1.104:8080/ws-snmp/';
+
+  const onConnected = () => {
+    console.log("Connected!!");
+    setTopics(['/topic/snmp-messages']);
+  }
+
+  const onDisconnected = () => {
+    console.log("Disconnected!");
+  }
+
+  const onMessageReceived = (msg) => {
+    console.log('New Message Received!!', msg);
+    setMessage(msg);
+  }
+
+  const date = new Date();
+
   return (
     <div className="App">
+      <SockJsClient 
+        url={SOCKET_URL}
+        topics={topics}
+        onConnect={onConnected}
+        onDisconnect={onDisconnected}
+        onMessage={onMessageReceived}
+        debug={true}
+      />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="App-button" onClick={onSendBtnClick}>Send</button>
+        {message && ((date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()) + ' >> ' + (((typeof message === 'string') && ('From Button >> ' + message)) || ('From Kafka Connector >> ' + message?.payload?.DEVICE_IP + ' >> ' + message?.payload?.VARIABLE)))}
       </header>
     </div>
   );
